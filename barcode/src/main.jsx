577f34ce525a65be5e11f06c2b6e6c5dc372107f
YriyProtcyshin <105303233+YriyProtcyshin@users.noreply.github.com>
2025-09-19 23:08:39 +0300

    commit

--- barcode/src/main.jsx
+++ barcode/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// 👇 импорт регистрации SW
+import { registerSW } from "virtual:pwa-register";
+
+// 👇 вызываем сразу при запуске
+registerSW({
+  immediate: true, // автообновление без диалога
+  onNeedRefresh() {
+    // если хочешь уведомление пользователю:
+    // if (confirm("Доступна новая версия, обновить?")) {
+    //   updateSW(true)
+    // }
+  },
+  onOfflineReady() {
+    console.log("Приложение готово для офлайна 🚀");
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
